114ed1634273a371fa3558a2992a9c8c6b61b2aa
Welliton gervickas <wellitogervickas@gmail.com>
2021-02-18 11:00:01 -0300

    initial structure

---
+++ index.test.js
@@ -0,0 +1,8 @@
+describe("Decimal to binary", () => {
+  const decimalToBinary = (integer) => {};
+
+  it("should return a valid binary value", () => {
+    expect(decimalToBinary(2)).toBe(10);
+    expect(decimalToBinary(9)).toBe(1001);
+  });
+});
